52f978f0a873cc35abc4275924e00b23046f958b
Pravin Rathod <medicogayatri3@gmail.com>
2026-07-06 19:08:33 +0530

    mouse follower feature is been added

--- script.js
+++ script.js
@@ -9,3 +9,11 @@ gsap.ticker.add((time) => {
 });
 
 gsap.ticker.lagSmoothing(0);
+
+function Mousefollower(){
+  window.addEventListener("mousemove",(det)=>{
+    document.querySelector(".mm").style.transform = `translate(${det.clientX}px,${det.clientY}px)`
+  })
+}
+
+Mousefollower()
